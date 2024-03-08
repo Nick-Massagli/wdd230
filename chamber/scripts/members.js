5380ba8b43fc17f1585f37cd0b960ff21c17b908
Nick-Massagli <mas19015@byui.edu>
2024-03-08 14:34:12 -0700

    Chamber Directory

---
+++ chamber/scripts/members.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://raw.githubusercontent.com/Nick-Massagli/wdd230/main/chamber/data/members.json'
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const members = jsonObject['members'];
+    const cards = document.querySelector('.cards');
+
+    members.forEach(member => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p')
+
+        h2.textContent = `${member.name}`;
+        p.textContent = `Address: ${member.address}`;
+        p2.textContent = `Phone Number(s): ${member.phone}`;
+        p3.textContent = `URL: ${member.url}`;
+        p4.textContent = `Membership Level: ${member.membership}`;
+        card.append(h2);
+        card.append(p);
+        card.append(p2);
+        card.append(p3);
+        card.append(p4);
+        cards.append(card);
+    });
+  });
