284708935de788f459c0bad760356f3612bbfe99
Nick-Massagli <mas19015@byui.edu>
2024-04-05 17:42:48 -0600

    Scoots Price Table start

--- scoots/scripts/table.js
+++ scoots/scripts/table.js
@@ -7,28 +7,29 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);
     const types = jsonObject['type'];
-    const cards = document.querySelector('.table');
+    const tables = document.querySelector('.table');
 
     types.forEach(type => {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let p = document.createElement('p');
-        let p2 = document.createElement('p');
-        let p3 = document.createElement('p');
-        let p4 = document.createElement('p');
-        let p5 = document.createElement('p');
+        let table = document.createElement('tr');
+        let th = document.createElement('th');
+        let td = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+        let td4 = document.createElement('td');
+        let td5 = document.createElement('td');
 
-        h2.textContent = `${type.name}`;
-        p.textContent = `Max Persons: ${type.maxpersons}`;
-        p2.textContent = `Reservation Half Day Price: ${type.reshday}`;
-        p3.textContent = `Reservation Full Day Price: ${type.resfday}`;
-        p4.textContent = `Walk-In Half Day Price: ${type.walkhday}`;
-        p5.textContent = `Walk-In Full Day Price: ${type.walkfday}`;
-        card.append(h2);
-        card.append(p);
-        card.append(p2);
-        card.append(p3);
-        card.append(p4);
-        cards.append(card);
+        th.textContent = `${type.name}`;
+        td.textContent = `Max Persons: ${type.maxpersons}`;
+        td2.textContent = `Reservation Half Day Price: ${type.reshday}`;
+        td3.textContent = `Reservation Full Day Price: ${type.resfday}`;
+        td4.textContent = `Walk-In Half Day Price: ${type.walkhday}`;
+        td5.textContent = `Walk-In Full Day Price: ${type.walkfday}`;
+        table.append(th);
+        table.append(td);
+        table.append(td2);
+        table.append(td3);
+        table.append(td4);
+        table.append(td5);
+        tables.append(table);
     });
   });
